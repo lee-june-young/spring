05b91d73cb61d52c594b25e1dae72498e5fc78dd
lee-june-young <uhiccup@gmail.com>
2023-07-23 21:09:06 +0900

    AppConfig 리팩터링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,23 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    //리팩토링: 메서드명을 보면 역할이 다 드러난다.
+    //그리고 함수 내부를 보면 구현을 뭘로 할껀지 다 드러난다.
+    //=> 설계에 대한 그림이 이 AppConfig 구성 정보에 그대로 그러난다.
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
