f528afacea5966c0b2614692c31adf8bcffd40b0
lee-june-young <uhiccup@gmail.com>
2023-07-24 00:45:52 +0900

    스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,36 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //리팩토링: 메서드명을 보면 역할이 다 드러난다.
     //그리고 함수 내부를 보면 구현을 뭘로 할껀지 다 드러난다.
     //=> 설계에 대한 그림이 이 AppConfig 구성 정보에 그대로 그러난다.
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
+
+//스프링으로 전환: @Configuration : 설정,구성정보를 담당한다는 뜻 @Bean: 얘네들이 다 스프링 컨테이너에 등록된다.
