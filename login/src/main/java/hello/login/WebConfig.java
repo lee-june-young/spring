b6d1af4713e8a8f95044b101974c5dcc5f50f1e9
lee-june-young <uhiccup@gmail.com>
2024-03-25 11:16:36 +0900

    서블릿 필터 - 인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,16 @@ public class WebConfig {
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1); //필터 순서
         filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2); //필터 순서
+        filterRegistrationBean.addUrlPatterns("/*"); //어짜피 WhiteList에서 거르고 있으므로
 
         return filterRegistrationBean;
     }
